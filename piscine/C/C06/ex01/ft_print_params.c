a2a5ca9797536868974b1d8ebd06308f6194b213
jdanelon <jdanelon@student.42sp.org.br>
2024-12-22 18:38:50 -0300

    added C06

---
+++ piscine/C/C06/ex01/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdanelon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/05 17:09:46 by jdanelon          #+#    #+#             */
+/*   Updated: 2019/12/05 17:49:20 by jdanelon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
---
+++ piscine/C/C06/ex03/ft_sort_params.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdanelon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/05 17:26:01 by jdanelon          #+#    #+#             */
+/*   Updated: 2019/12/06 15:45:39 by jdanelon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strcmp(char *str1, char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] != '\0')
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (str1[i] - str2[i]);
+}
+
+char	**ft_sort_params(int size, char **str)
+{
+	int		i;
+	char	*aux;
+
+	i = 1;
+	while (i + 1 < size)
+	{
+		if (ft_strcmp(str[i], str[i + 1]) > 0)
+		{
+			aux = str[i];
+			str[i] = str[i + 1];
+			str[i + 1] = aux;
+			i = 0;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	ft_sort_params(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
